f465a259a89592c0994ca4553b9f53603d113039
marcusaloise <marcusaloise@gmail.com>
2022-02-11 09:22:11 -0400

    Terminando a o modulo 06 de controle de fluxo

--- 18.py
+++ 18.py
@@ -1,2 +1,15 @@
 # while loop
+ 
+valor = 100
+dia = 1
+
+
+while valor > 20:
+    print(f'No dia {dia} o produto vai ser vendido por {valor}')
+    #print(dia)
+    dia += 1
+    #print(valor)
+    valor -= 5
+    
+
 
---
+++ 19.py
@@ -0,0 +1,9 @@
+# criando condições com While Loop
+
+valor = int(input('Digite o valor do seu produto em R$: '))
+
+
+while valor > 20:
+    valor = (valor * 0.10) + valor
+    print(f' O valor final do seu produto será de R${valor}')
+    break
