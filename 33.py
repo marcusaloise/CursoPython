d377d09c3941132ff65451d4d5ea16ddd9aa7840
marcusaloise <marcusaloise@gmail.com>
2022-02-24 00:21:03 -0400

    .

---
+++ 33.py
@@ -0,0 +1,7 @@
+# Tuples
+    # Armazena mais de uma informação em váriaveis
+    # manter a sequencia dps dados em uma variável
+    # não podem ser alteradas (Immutable)
+
+cores_list = ['Amarelo','verde','azul','vermelho']
+cores_tuple = ('Amarelo','verde','azul','vermelho')
